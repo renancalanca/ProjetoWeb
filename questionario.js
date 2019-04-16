7ac371fd7f64e6d5994d7034f702dbd7b844274c
Renan Campregher <renan.calanca@hotmail.com>
2019-04-16 16:51:19 -0300

    Atualizações

--- questionario.js
+++ questionario.js
@@ -6,19 +6,36 @@ function loadXMLDoc() {
     }
   };
   xmlhttp.open("GET", "questionario.xml", true);
+
   xmlhttp.send();
 }
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  var x = xmlDoc.getElementsByTagName("CD");
+  var x = xmlDoc.getElementsByTagName("question");
+  var lstQuestions = [];
   for (i = 0; i < x.length; i++) {
-    var div = document.createElement("div");
-    console.log(x[i].getAttribute("id"));
-    div.id = x[i].getAttribute("id");
+    // lstQuestions.push(
+    //   {
+    //     id: x[i].getAttribute("id"),
+    //     text: x[i].getElementsByTagName("text"),
+    //     questions: x[i].getElementsByTagName("answer")
+    //   }
+    // );
     //div.className = colocar uma classe de css
-    var txt = "";
-    x[i].getElementsByTagName("text")[0].childNodes[0].nodeValue;
+    // txt += x[i].getElementsByTagName("text")[0].childNodes[0].nodeValue;
+    //document.getElementsByTagName("body")[0].appendChild(iDiv);
+    var iDiv = document.createElement("div");
+    iDiv.id = x[i].getAttribute("id");
+    var title = document.createElement("h3");
+    title.textContent = x[i].getElementsByTagName("text");
+    var input = document.createElement("INPUT");
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", "answer");
+    input.setAttribute("value", "0"/*pegar o valor do que ta vindo do xml */);
+    iDiv.innerHTML = title;
+    iDiv.innerHTML = input;    
+    console.log(x[i].getAttribute("id"));
   }
-  document.getElementById("demo").innerHTML = table;
+  document.getElementById("main").innerHTML = txt;
 }
