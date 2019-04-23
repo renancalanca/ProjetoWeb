0d386a6c476aa9b125b238699a2eaeb0bed72fdf
Renan Campregher <renan.calanca@hotmail.com>
2019-04-23 16:51:47 -0300

    Ajustes

--- questionario.js
+++ questionario.js
@@ -1,17 +1,38 @@
+function getXml(){
+  // var texto = document.getElementById("txtXml").value.toString();
+  // console.log(texto);
+
+  var stringxml = 
+  "<question>" +
+  "<title>Qual o valor de 2+3?</title>"+
+  "<answer>1</answer>" + 
+  "<answer>2</answer>" +
+  "<answer>3</answer>" +
+  "<answer>4</answer>" +
+  "<answer>5</answer>" +
+  "</question>";
+
+  var parser = new DOMParser();
+  var oDom = parser.parseFromString(stringxml, "text/xml");
+  console.log(oDom.getElementsByTagName("title")[0].childNodes[0].nodeValue);
+  convertXmlToQuestion(oDom);
+}
+
+//Essa função será usada para quando estiver usando servidor
 function loadXMLDoc() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
+      var xmlDoc = this.responseXML;
+      convertXmlToQuestion(xmlDoc);
     }
   };
   xmlhttp.open("GET", "questionario.xml", true);
-
   xmlhttp.send();
 }
-function myFunction(xml) {
+
+function convertXmlToQuestion(xml) {
   var i;
-  var xmlDoc = xml.responseXML;
   var x = xmlDoc.getElementsByTagName("question");
   var lstQuestions = [];
   for (i = 0; i < x.length; i++) {
