6f55cfdf8ca732e489c1a003f23b1cd058dba744
Renan Campregher <renan.calanca@hotmail.com>
2019-05-22 15:51:04 -0300

    Att

---
+++ javascript/jqueryQst.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  console.log( "ready!" );
+});
+
+// function todosChecados2() {
+//   var lstQuestionAnswered = $("question");
+//   console.log(lstQuestionAnswered.length);
+
+//   var qtdChecked = 0;
+//   $("input:checkbox[name=type]:checked").each(function() {
+//     yourArray.push($(this).val());
+//     qtdChecked++;
+//   });
+
+//   if (qtdChecked == lstQuestionAnswered.length) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// $(document).ready(function() {
+//   $.getJSON("../xml/questionario.json", function(data) {
+//     console.log(data);
+//     // var items = [];
+//     // $.each( data, function( key, val ) {
+//     //   items.push(val);
+//     //   console.log(key);
+//     // });
+//   });
+// });
